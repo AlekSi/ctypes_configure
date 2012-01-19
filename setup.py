6dead021f7f2ed4e33cf5ff155da141ca1d56ebf
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2012-01-19 17:30:53 +0400

    Import ctypes_configure 0.1 from PyPI.
    http://pypi.python.org/pypi/ctypes_configure/0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ctypes_configure',
+      version='0.1',
+      description='Ctypes Configuration',
+      author='Maciej Fijalkowski',
+      author_email='fijall@gmail.com',
+      url='http://codespeak.net/svn/pypy/dist/ctypes_configure/doc/configure.html',
+      packages=['ctypes_configure', 'ctypes_configure/test'],
+     )
