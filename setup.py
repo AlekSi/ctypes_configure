c818f94802946bf844529e66f2d8ca97788e7352
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2012-11-13 13:29:00 +0400

    Clean-up package info.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ctypes_configure2',
-      version='0.4',
+      version='0.4.2',
       description='Ctypes Configuration',
       author='Maciej Fijalkowski',
       author_email='fijall@gmail.com',
@@ -11,5 +11,5 @@ setup(name='ctypes_configure2',
       maintainer_email='alexey.palazhchenko@gmail.com',
       url='http://codespeak.net/~fijal/configure.html',
       packages=['ctypes_configure', 'ctypes_configure/test'],
-      license='LICENSE',
+      license='MIT',
      )
